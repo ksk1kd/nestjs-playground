727281d8e84fd80bc9c9101c6885d878f62fe175
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-04-01 23:43:58 +0900

    add static route

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, Req } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}

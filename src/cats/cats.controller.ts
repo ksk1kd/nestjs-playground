0059326e75837f62f8188f7f992c99e44c20397b
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-04-08 22:31:02 +0900

    add pipe

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 
@@ -8,14 +15,14 @@ export class CatsController {
 
   @Get()
   async findAll(
-    @Query('age') age: number,
-    @Query('breed') breed: string,
+    @Query('age', new DefaultValuePipe(0), ParseIntPipe) age: number,
+    @Query('breed', new DefaultValuePipe('')) breed: string,
   ): Promise<Cat[]> {
     return this.catsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): string {
+  findOne(@Param('id', ParseIntPipe) id: string): string {
     return `This action returns a #${id} cat`;
   }
 }
